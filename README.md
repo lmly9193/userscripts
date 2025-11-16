c529b89d374754358504c39e0c9be1deea49f1da
lmly9193 <15520160+lmly9193@users.noreply.github.com>
2025-11-16 19:42:30 +0800

    update custom-enhancement.user.js
    重新命名所有使用者腳本(加上.user.js)

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 一些自用的使用者脚本，沒有義務提供任何修正或更新！
 
+- [ScriptCat](https://chromewebstore.google.com/detail/scriptcat/ndcooeababalnlpkfedmmbbbgkljhpjf)
 - [Tampermonkey](https://chrome.google.com/webstore/detail/dhdgffkkebhmkfjojejmpbldmpobfkfo)
 - [Violentmonkey](https://chrome.google.com/webstore/detail/jinjaccalgkegednnccohejagnlnfdag)
---
+++ custom-enhancement.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         JavDB Custom Enhancement
+// @namespace    https://github.com/lmly9193/userscripts
+// @version      1.0.0
+// @author       lmly9193
+// @description  增加該網站的自訂義功能
+// @license      MIT
+// @icon         https://javdb.com/favicon-32x32.png
+// @match        https://javdb.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/lmly9193/userscripts/main/custom-enhancement.user.js
+// @updateURL    https://raw.githubusercontent.com/lmly9193/userscripts/main/custom-enhancement.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const pathname = window.location.pathname;
+
+    // https://javdb.com/v/*
+    if (pathname.startsWith('/v/')) {
+        const cpElement = document.querySelector('.copy-to-clipboard');
+        const vid = cpElement ? cpElement.dataset.clipboardText : null;
+        const moviePanelInfo = document.querySelector('.movie-panel-info');
+        if (vid && moviePanelInfo) {
+            const newPanel = createMovieLinksPanel(vid);
+            moviePanelInfo.appendChild(newPanel);
+        }
+    }
+
+    // https://javdb.com/actors/*
+    if (pathname.startsWith('/actors/')) {
+        const els = document.querySelectorAll('.movie-list .video-title > strong');
+        const uids = Array.from(els).map(el => el.textContent);
+        const query = uids.join('|');
+        const sectionAddition = document.querySelector('.section-addition');
+        if (sectionAddition) {
+            const columns = sectionAddition.closest('.columns');
+            const newColumn = createSearchAllButtonColumn(query);
+            columns.appendChild(newColumn);
+        }
+    }
+
+    function createMovieLinksPanel(query) {
+        const panelBlock = document.createElement('div');
+        panelBlock.className = 'panel-block';
+        panelBlock.innerHTML = `
+            <div class="columns">
+                <div class="column">
+                    <div class="buttons are-small review-buttons">
+                        <a class="button is-warning is-outlined" href="es://${query}">在電腦搜尋</a>
+                    </div>
+                </div>
+            </div>`;
+        return panelBlock;
+    }
+
+    function createSearchAllButtonColumn(query) {
+        const columnDiv = document.createElement('div');
+        columnDiv.className = 'column';
+        columnDiv.innerHTML = `<p><a class="button is-warning" style="display: block" href="es://regex%3A${encodeURIComponent(query)}">在電腦搜尋全部</a></p>`;
+        return columnDiv;
+    }
+
+})();
